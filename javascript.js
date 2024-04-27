85a8d0f3d20e7de39fd978cff47e6c1a0b08c0f2
Thiago Walek <118086425+Trexxloopp@users.noreply.github.com>
2024-04-27 15:40:43 -0300

    responsividade feita, e som de click adicionado

--- javascript.js
+++ javascript.js
@@ -4,9 +4,15 @@ function acende() {
     a.style.backgroundColor = "yellow";
     a.style.boxShadow = "1px 1px 80px white"
     console.log("Acendeu!!!")
+    var sound = new Audio();
+    sound.src = "mouse-sound.mp3";
+    sound.play();
 }
 function apaga() {
     a.style.backgroundColor = "#191815";
-    a.style.boxShadow = "0px 0px 0px white"
-    console.log("Apagou!!!")
+    a.style.boxShadow = "0px 0px 0px white";
+    console.log("Apagou!!!");
+    var sound = new Audio();
+    sound.src = "mouse-sound.mp3";
+    sound.play();
 }
